3bf892bfc04801a36cc11298f52bac82b9833622
FrancoMaxime <franco.maxime@gmail.com>
2018-05-20 20:33:37 +0200

    jouer avec le https

--- controller.py
+++ controller.py
@@ -1,4 +1,5 @@
 import web
+from web.wsgiserver import CherryPyWSGIServer
 
 urls = (
     '/', 'index'
@@ -10,4 +11,8 @@ class index:
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
+    ssl_cert = '/etc/letsencrypt/live/maximefranco.be/cert.pem'
+    ssl_key = '/etc/letsencrypt/live/maximefranco.be/privkey.pem'
+    CherryPyWSGIServer.ssl_certificate = ssl_cert
+    CherryPyWSGIServer.ssl_private_key = ssl_key
     app.run()
